397d576d89cbec7764fd116faf85934762e86b21
Wonko <wonko@hanstool.org>
2015-11-02 22:12:20 +0100

    cli: organize code, configurable command prompt

--- ht3/cli.py
+++ ht3/cli.py
@@ -13,89 +13,6 @@ try:
 except:
     readline = None
 
-def setup_readline():
-    if not readline:
-        return False
-
-    history = Env.get('RL_HISTORY', None)
-    if history:
-        history = os.path.expanduser(history)
-        try:
-            readline.read_history_file(history)
-        except FileNotFoundError:
-            open(history, 'wb').close()
-        hlen = readline.get_history_length()
-
-        def save():
-            newhlen = readline.get_current_history_length()
-            l = newhlen - hlen
-            if hasattr(readline, 'append_history_file'):
-                readline.append_history_file(l, history)
-            else:
-                readline.write_history_file(history)
-        atexit.register(save)
-
-    completion_cache=[]
-
-    def rl_complete(text, n):
-        nonlocal completion_cache
-        if n == 0:
-            try:
-                completion_cache = list(lib.get_completion(text))
-            except:
-                traceback.print_exc()
-            #print("\nCompletion of %s, cache: %s\n--> %s" % (text, completion_cache, text),end='')
-        return completion_cache[n]
-    readline.set_completer(rl_complete)
-    readline.set_completer_delims('')
-    readline.parse_and_bind('tab: complete')
-
-
-# API
-
-@Env
-def show(s, *args, **kwargs):
-    print (str(s) % args)
-
-@Env
-def log(s, *args, **kwargs):
-    print (str(s) % args)
-
-@Env
-def edit_file(path, line):
-    execute(EDITOR, f)
-
-Env['help'] = help
-
-Env.INTERFACE = "ht3.cli"
-
-def repl():
-    rl = setup_readline()
-    try:
-        while 1:
-            try:
-                s = input("ht> ")
-            except KeyboardInterrupt:
-                return 127
-            except EOFError:
-                return 0
-            else:
-                if s:
-                    try:
-                        x = lib.run_command(s)
-                    except KeyboardInterrupt:
-                        print("\n!!Aborted")
-                    except SystemExit:
-                        raise
-                    except:
-                        traceback.print_exc()
-                    else:
-                        if x:
-                            print(x)
-    finally:
-        print()
-    return 0
-
 def main(args):
     done = False
     help = False
@@ -145,6 +62,94 @@ def main(args):
     if not done:
         result = repl()
     return result
+
+def repl():
+    setup_readline()
+    try:
+        while 1:
+            try:
+                s = input(Env.CLI_PROMPT())
+            except KeyboardInterrupt:
+                return 127
+            except EOFError:
+                return 0
+            else:
+                if s:
+                    try:
+                        x = lib.run_command(s)
+                    except KeyboardInterrupt:
+                        print("\n!!Aborted")
+                    except SystemExit:
+                        raise
+                    except:
+                        traceback.print_exc()
+                    else:
+                        if x:
+                            print(x)
+    finally:
+        print()
+    return 0
+
+def setup_readline():
+    if not readline:
+        return False
+
+    history = Env.get('RL_HISTORY', None)
+    if history:
+        history = os.path.expanduser(history)
+        try:
+            readline.read_history_file(history)
+        except FileNotFoundError:
+            open(history, 'wb').close()
+        hlen = readline.get_history_length()
+
+        def save():
+            newhlen = readline.get_current_history_length()
+            l = newhlen - hlen
+            if hasattr(readline, 'append_history_file'):
+                readline.append_history_file(l, history)
+            else:
+                readline.write_history_file(history)
+        atexit.register(save)
+
+    completion_cache=[]
+
+    def rl_complete(text, n):
+        nonlocal completion_cache
+        if n == 0:
+            try:
+                completion_cache = list(lib.get_completion(text))
+            except:
+                traceback.print_exc()
+            #print("\nCompletion of %s, cache: %s\n--> %s" % (text, completion_cache, text),end='')
+        return completion_cache[n]
+    readline.set_completer(rl_complete)
+    readline.set_completer_delims('')
+    readline.parse_and_bind('tab: complete')
+
+
+@Env
+def CLI_PROMPT():
+    return "ht3> "
+
+# API
+
+@Env
+def show(s, *args, **kwargs):
+    print (str(s) % args)
+
+@Env
+def log(s, *args, **kwargs):
+    print (str(s) % args)
+
+@Env
+def edit_file(path, line):
+    execute(EDITOR, f)
+
+Env['help'] = help
+
+Env['INTERFACE'] = "ht3.cli"
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
