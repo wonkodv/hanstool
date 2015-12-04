fc51b1b5c816c2ed09d3297e64f57106ade7ae11
Wonko <wonko@hanstool.org>
2015-12-04 16:58:45 +0100

    Env/Windows: Only load windows specific modules if on win
    needed there because unittest loads the package no matter what.

--- ht3/env/windows/__init__.py
+++ ht3/env/windows/__init__.py
@@ -1,3 +1,7 @@
-from . import message_box
-from . import default
-from . import hwnds
+
+from ht3.lib import Check
+
+if Check.os.win:
+    from . import message_box
+    from . import default
+    from . import hwnds
