72aa1cf730c19c966d429b0301f4f86cbc3d71cf
Wonko <wonko@hanstool.org>
2016-01-06 13:34:12 +0100

    default_scripts add example for VirtualBox

--- ht3/default_scripts/examples.50.py
+++ ht3/default_scripts/examples.50.py
@@ -18,3 +18,28 @@ def timer(t:float=3):
     """ Tea timer """
     sleep(t*60)
     MessageBox("Timer", "Done (%.1f)"%t, "TOPMOST SETFOREGROUND SYSTEMMODAL OK ICONINFORMATION")
+
+
+def _vb_get_vms(s=None):
+    """Helper function for the vb command, get the names of installed boxes."""
+    import subprocess
+    p = subprocess.Popen(
+        ["vboxmanage", "list", "vms"],
+        universal_newlines=True,
+        stdout=subprocess.PIPE)
+    output, _ = p.communicate()
+    for s in sorted(output.split('\n')):
+        x = s.split()
+        if x:
+            yield x[0][1:-1]
+
+@cmd(args=1,complete=_vb_get_vms)
+def vb(box=None):
+    """Open VirtualBox (the manager) or start a box with the approximate name."""
+    if not box:
+        execute("virtualbox")
+    else:
+        import difflib
+        boxes = _vb_get_vms()
+        box = difflib.get_close_matches(box, boxes, 1, 0.1)[0]
+        execute("vboxmanage", "startvm", box)
