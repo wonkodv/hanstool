54419b7d34e1a8866ef6cf0b09f97b360eaa3527
Wonko <wonko@hanstool.org>
2016-03-13 18:58:23 +0100

    win/execute: get full path for executable

--- ht3/utils/windows/execute.py
+++ ht3/utils/windows/execute.py
@@ -8,42 +8,53 @@ from ht3.env import Env
 
 def execute(exe, *args, **kwargs):
     """Find an executable in PATH, optionally appending PATHEXT extensions, then execute."""
-    exe, ext = next(_get_exe_path(exe))
+    exe = next(_get_exe_path(exe, True))
     return process.execute(exe, *args, **kwargs)
 
 _extensions = os.environ.get('PATHEXT','').split(os.pathsep)
 _paths = [pathlib.Path(p) for p in os.get_exec_path()]
 
-def _get_exe_path_ext(p):
+
+
+def _get_exe_path_ext(p, full_path):
     if p.suffix:
         for c in p.parent.glob(p.name+'*'):
-            yield str(c), ''
+            if full_path:
+                yield str(c)
+            else:
+                yield str(c), ''
     else:
         for ext in _extensions:
             for c in p.parent.glob(p.name+'*'+ext):
-                yield c.name, ext
+                if full_path:
+                    yield str(c)
+                else:
+                    yield c.name, ext
 
-def _get_exe_path(s):
+def _get_exe_path(s, full_path):
     p = pathlib.Path(s)
     parts = p.parts
     if len(parts) > 1:
-        for c in _get_exe_path_ext(p):
+        for c in _get_exe_path_ext(p, full_path):
             yield c
     else:
         for c in Env.get('PATH',_paths):
             f = c / s
-            for x in _get_exe_path_ext(f):
+            for x in _get_exe_path_ext(f, full_path):
                 yield x
 
 def complete_executable(s):
-    """Find all possible executables in PATH, optionally appending PATHEXT."""
+    """Find all possible executables in PATH, optionally appending PATHEXT.
+
+    If there is more than file with the same name, only differing in the 
+    extension, yield both, else yield only the name"""
     s = shlex.split(s)
     if len(s) != 1:
         return
     s = s[0]
 
     values = {}
-    for exe, ext in _get_exe_path(s):
+    for exe, ext in _get_exe_path(s, False):
         short = exe[:-len(ext)]
         if short in values:
             values[short].add(exe)
