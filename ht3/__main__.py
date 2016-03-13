5739ba29a6497517a0bc2e0a27ac34f983e97cb7
Wonko <wonko@hanstool.org>
2016-03-13 18:20:34 +0100

    main: dont debug SystemExit and KeyboardInterrupt

--- ht3/__main__.py
+++ ht3/__main__.py
@@ -73,6 +73,8 @@ def main(args):
         if not _r and _f:
             lib.run_frontends()
         return 0
+    except (KeyboardInterrupt, SystemExit):
+        raise
     except:
         if Env.get('DEBUG',False):
             import traceback
