5fd79705949cc107e86ceedff4dc69a1f154cdaa
Wonko <wonko@hanstool.org>
2016-11-26 10:39:04 +0100

    utils: add `cache_for` decorator
    caches a function's result for a given time

--- ht3/utils/helpers.py
+++ ht3/utils/helpers.py
@@ -4,6 +4,7 @@ import textwrap
 import functools
 import shlex
 import inspect
+import time
 
 from ht3.lib import evaluate_py_expression
 from ht3.env import Env
@@ -29,4 +30,30 @@ def cmd_func(name, func, *args, **kwargs):
 
     return cmdf
 
-__all__  = 'cmd_func',
+
+def cache_for(t):
+    """Decorator that caches a function result for `t` seconds.
+
+    the function can not have arguments.
+
+        @cache_for(1.25)
+        def f()
+            return complicated_semi_time_dependant_stuff(with_args)
+    """
+    float(t)
+    def deco(f):
+        updated = 0
+        cache = None
+        @functools.wraps(f)
+        def wrapper():
+            nonlocal cache
+            nonlocal updated
+            now = time.monotonic()
+            if now - updated > t:
+                updated = now
+                cache = f()
+            return cache
+        return wrapper
+    return deco
+
+__all__  = 'cmd_func', 'cache_for'
---
+++ ht3/utils/test_helpers.py
@@ -0,0 +1,18 @@
+import time
+from . import helpers
+
+def test_cache_for():
+    x=0
+    @helpers.cache_for(0.05)
+    def f():
+        nonlocal x
+        x = x+1
+        return x
+
+    assert f.__name__ == 'f'
+    assert f() == 1
+    assert f() == 1
+    assert f() == 1
+    time.sleep(0.1)
+    assert f() == 2
+    assert f() == 2
