75de47b12850f51e9293a558f12706392095a3de
Wonko <wonko@hanstool.org>
2018-01-20 19:10:31 +0100

    utils/process: fix tests for shell and env vars on win

--- ht3/utils/test_process.py
+++ ht3/utils/test_process.py
@@ -1,7 +1,7 @@
 import subprocess
-import pytest
 import sys
 import os
+import unittest
 from .process import execute, shellescape
 
 
@@ -11,38 +11,54 @@ def helper(*args,**kwargs):
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         universal_newlines=True,
+        errors="replace",
         env=dict([('TEST','hans')] + list(os.environ.items())),
         **kwargs
     )
     return p
 
-def test_excecute():
-    p = helper(sys.executable, "-c", "print('$TEST')")
-    t, e = p.communicate()
-    assert t.strip() == '$TEST'
+class TestProcess(unittest.TestCase):
+    if sys.platform == 'win32':
+        def test_excecute(self):
+            p = helper(sys.executable, "-c", "print('%TEST%')")
+            t, e = p.communicate()
+            assert t.strip() == '%TEST%'
 
-def test_excecute_split():
-    p = helper(sys.executable, "-c", "print('$TEST')",is_split=True)
-    t, e = p.communicate()
-    assert t.strip() == '$TEST'
+        def test_excecute_split(self):
+            p = helper(sys.executable, "-c", "print('%TEST%')",is_split=True)
+            t, e = p.communicate()
+            assert t.strip() == '%TEST%'
 
-def test_excecute_shell():
-    if sys.platform=='win32':
-        pytest.skip("Meh") # TODO: make this test windows safe
-    p = helper(shellescape(sys.executable) + " -c \"print('$TEST')\"", shell=True)
-    t, e = p.communicate()
-    assert t.strip() == 'hans'
+        def test_excecute_shell(self):
+            p = helper('"'+sys.executable + '" -c "print(\'%TEST%\')"', shell=True)
+            t, e = p.communicate()
+            assert t.strip() == 'hans', [t,e,p]
+    else:
+        def test_excecute(self):
+            p = helper(sys.executable, "-c", "print('$TEST')")
+            t, e = p.communicate()
+            assert t.strip() == '$TEST'
 
-def test_excecute_nosplit_err():
-    with pytest.raises(TypeError):
-        helper("a","b", is_split=False)
+        def test_excecute_split(self):
+            p = helper(sys.executable, "-c", "print('$TEST')",is_split=True)
+            t, e = p.communicate()
+            assert t.strip() == '$TEST'
 
-def test_excecute_shell_split():
-    with pytest.raises(TypeError):
-        helper("a","b", is_split=True, shell=True)
+        def test_excecute_shell(self):
+            p = helper(shellescape(sys.executable) + " -c \"print('$TEST')\"", shell=True)
+            t, e = p.communicate()
+            assert t.strip() == 'hans'
 
-def test_excecute_shell_split2():
-    with pytest.raises(TypeError):
-        helper("a","b", shell=True)
+    def test_excecute_nosplit_err(self):
+        with self.assertRaises(TypeError):
+            helper("a","b", is_split=False)
+
+    def test_excecute_shell_split(self):
+        with self.assertRaises(TypeError):
+            helper("a","b", is_split=True, shell=True)
+
+    def test_excecute_shell_split2(self):
+        with self.assertRaises(TypeError):
+            helper("a","b", shell=True)
 
 
