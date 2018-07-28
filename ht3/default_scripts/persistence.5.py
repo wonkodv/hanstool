174929fdbd3475487fb6447a7c3e9c2d4f1ff1b2
Wonko <wonko@hanstool.org>
2018-07-28 15:25:30 +0200

    default-scripts: Content of dict Env[Per] is un-pickled on read/write

---
+++ ht3/default_scripts/persistence.5.py
@@ -0,0 +1,43 @@
+import Env
+
+import pickle
+import pathlib
+
+class Persistence:
+    dict = None
+
+    def path(self):
+        return pathlib.Path(Env.get('PERSISTENCE_FILE','~/.config/ht3/persist')).expanduser()
+
+    def load(self):
+        if self.dict is not None:
+            return
+        try:
+            with self.path().open("rb") as f:
+                self.dict = pickle.load(f)
+        except Exception: # forget everything on the slightest error
+            self.dict = {}
+
+    def save(self):
+        assert self.dict is not None
+        with self.path().open("wb") as f:
+            pickle.dump(self.dict, f)
+
+    def set(self,key,val):
+        self.load()
+        self.dict[key] = val
+        self.save()
+    __setitem__ = set
+
+    def __getitem__(self, key):
+        self.load()
+        return self.dict[key]
+
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+
+Env['Per'] = Persistence()
