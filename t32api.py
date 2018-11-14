7e3693d165077565a53877d7563a3832a2140f51
wonko <wonko@hanstool.org>
2018-11-14 12:22:14 +0100

    T32API: load library when needed

--- t32api.py
+++ t32api.py
@@ -7,10 +7,10 @@ http://www2.lauterbach.com/pdf/api_remote.pdf
 from pathlib import Path
 import sys
 from ctypes import CFUNCTYPE, POINTER, byref, c_char, c_char_p, c_int, c_ulonglong, c_voidp, cdll, sizeof
+import itertools
 
 class T32Error(Exception):
     pass
-
 
 try:
     import Env
@@ -19,54 +19,67 @@ try:
     def t32cmd(cmd:str):
         with Connection() as c:
             c.do(cmd)
-            
 except ImportError:
     Env = None
     # Not HT3
 
-def lib_path():
-    if Env:
-        try:
-            return Env['T32_LIB']
-        except KeyError:
-            pass
-    if sizeof(c_voidp)==4:    # 32 Bit
-        libs = ("t32api.dll", "t32api.so",)
-    else:
-        libs = ("t32api64.dll", "t32api64.so", )
-    for lib in libs:
-        for d in ("", Path(__file__).parent, r"C:\T32\demo\api\capi\dll"):
-            f = Path(d) / lib
-            if f.exists():
-                return f
-    raise FileNotFoundError("No T32api lib found")
+class T32API():
+    def __init__(self):
+        lib = None
+        if Env:
+            try:
+                p = Env['T32_LIB']
+                lib  = cdll.LoadLibrary(str(p))
+            except KeyError:
+                pass
+        if sizeof(c_voidp)==4:    # 32 Bit
+            libs = ("t32api.dll", "t32api.so",)
+        else:
+            libs = ("t32api64.dll", "t32api64.so", )
+        for d, n in itertools.product( ("", Path(__file__).parent, r"C:\T32\demo\api\capi\dll"), libs):
+                p = str(Path(d) / n)
+                try:
+                    lib  = cdll.LoadLibrary(p)
+                    break
+                except OSError as e:
+                    pass
+        if lib is None:
+            raise FileNotFoundError("No T32api lib found")
 
-class Connection:
-    p = lib_path()
-    t32api  = cdll.LoadLibrary(str(p))
+        self.lib = lib
 
-    def _errcheck(result, func, args):
+        self.Config = CFUNCTYPE(c_int, c_char_p, c_char_p)(lib.T32_Config)
+        self.Config.errcheck = self._errcheck
+        self.Init   = CFUNCTYPE(c_int)(lib.T32_Init)
+        self.Init.errcheck = self._errcheck
+        self.Attach   = CFUNCTYPE(c_int, c_int)(lib.T32_Attach)
+        self.Attach.errcheck = self._errcheck
+        self.Ping   = CFUNCTYPE(c_int)(lib.T32_Ping)
+        self.Ping.errcheck = self._errcheck
+        self.Cmd   = CFUNCTYPE(c_int, c_char_p)(lib.T32_Cmd)
+        self.Cmd.errcheck = self._errcheck
+        self.GetMessage   = CFUNCTYPE(c_int, c_char_p, POINTER(c_ulonglong))(lib.T32_GetMessage)
+        self.GetMessage.errcheck = self._errcheck
+        self.Exit   = CFUNCTYPE(c_int)(lib.T32_Exit)
+        self.Exit.errcheck = self._errcheck
+
+    def _errcheck(self, result, func, args):
         if result != 0:
             raise T32Error("Error in T32API: %d" %(result, ), func, args, result)
-    T32_Config = CFUNCTYPE(c_int, c_char_p, c_char_p)(t32api.T32_Config)
-    T32_Config.errcheck = _errcheck
-    T32_Init   = CFUNCTYPE(c_int)(t32api.T32_Init)
-    T32_Init.errcheck = _errcheck
-    T32_Attach   = CFUNCTYPE(c_int, c_int)(t32api.T32_Attach)
-    T32_Attach.errcheck = _errcheck
-    T32_Ping   = CFUNCTYPE(c_int)(t32api.T32_Ping)
-    T32_Ping.errcheck = _errcheck
-    T32_Cmd   = CFUNCTYPE(c_int, c_char_p)(t32api.T32_Cmd)
-    T32_Cmd.errcheck = _errcheck
-    T32_GetMessage   = CFUNCTYPE(c_int, c_char_p, POINTER(c_ulonglong))(t32api.T32_GetMessage)
-    T32_GetMessage.errcheck = _errcheck
-    T32_Exit   = CFUNCTYPE(c_int)(t32api.T32_Exit)
-    T32_Exit.errcheck = _errcheck
+
+    @classmethod
+    def instance(cls):
+        i = cls()
+        cls.instance = lambda:i
+        return i
+
+class Connection:
 
     def __init__(self, host='localhost', port=20000, packetlen=1024):
         self.host = host
         self.port = port
         self.packetlen = packetlen
+        self.api = T32API.instance()
 
     def __enter__(self):
         self.init()
@@ -78,20 +91,20 @@ class Connection:
 
 
     def init(self):
-        self.T32_Config(b"NODE=", self.host.encode("ASCII"))
-        self.T32_Config(b"PORT=", str(self.port).encode("ASCII"))
-        self.T32_Config(b"PACKLEN=", str(self.packetlen).encode("ASCII"))
-        self.T32_Init()
-        self.T32_Attach(1)
+        self.api.Config(b"NODE=", self.host.encode("ASCII"))
+        self.api.Config(b"PORT=", str(self.port).encode("ASCII"))
+        self.api.Config(b"PACKLEN=", str(self.packetlen).encode("ASCII"))
+        self.api.Init()
+        self.api.Attach(1)
 
     def do(self, cmd):
-        self.T32_Cmd(cmd.encode("ASCII"))
+        self.api.Cmd(cmd.encode("ASCII"))
 
     def get_message(self):
         raise NotImplementedError("Buggy")
         buf = (c_char * 300)()
         status = c_ulonglong(0)
-        self.T32_GetMessage(buf, byref(status))
+        self.api.GetMessage(buf, byref(status))
         v = status.value
         if v:
             typ = [m for i,m in self._message_codes.items() if v&i]
@@ -99,10 +112,10 @@ class Connection:
             return typ, msg
 
     def ping(self):
-        self.T32_Ping()
+        self.api.Ping()
 
     def close(self):
-        self.T32_Exit()
+        self.api.Exit()
 
 if __name__ == '__main__':
     with Connection() as c:
