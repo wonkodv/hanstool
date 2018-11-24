5e9cce62ed827d12304b63bfa37fa42a8729ef22
Wonko <wonko@hanstool.org>
2018-11-24 12:51:37 +0100

    dafault_scripts: return more results, for ht3client

--- ht3/default_scripts/executables.45.py
+++ ht3/default_scripts/executables.45.py
@@ -5,18 +5,21 @@ import shlex
 @cmd(name="$")
 def _procio(cmd:args.ExecutableWithArgs):
     """Show output of a shell command."""
-    show(procio(cmd, shell=True, is_split=False))
+    out = procio(cmd, shell=True, is_split=False)
+    show(out)
+    return out
 
 @cmd(name="#")
 def _execute(cmd:args.ExecutableWithArgs):
     """Execute a Program and wait for completion."""
     p = execute(cmd, is_split=False)
-    p.wait()
+    return p.wait()
 
 @cmd(name="&")
 def _execute_bg(cmd:args.ExecutableWithArgs):
     """Execute a Program and let it run in background."""
     p = execute_disconnected(cmd, is_split=False)
+    return p
 
 @cmd(name="")
 def _execute_auto(cmd:args.ExecutableWithArgs):
@@ -35,4 +38,6 @@ def _executable_command_h(command_string):
 
 @cmd(name="which")
 def _which(cmd:args.Executable):
-    show(which(cmd))
+    p = which(cmd)
+    show(p)
+    return p
