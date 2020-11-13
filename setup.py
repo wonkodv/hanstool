1ae6af6d9c0fdf03d4f03f7ec5be2bbebf7012a4
Wonko <wonko@hanstool.org>
2020-11-13 13:42:29 +0100

    setup: use cfg

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-#!/usr/bin/env python
-"""Install ht3."""
+import setuptools
+setuptools.setup()
 
-from setuptools import setup, find_packages
-
-setup(  name='ht3',
-        version='0.2.0',
-        description='Shell like Python interface',
-        license='GPLv2',
-        author='Wonko',
-        author_email='wonko@hanstool.org',
-        url='https://github.com/wonkodv/hanstool',
-        packages=find_packages(),
-        package_data={
-            'ht3': [
-                'default_scripts/*.py',
-                'test_scripts/*.py',
-            ]
-        },
-        entry_points={
-            'console_scripts': [
-                'ht=ht3.__main__'
-            ]
-        },
-    )
