8a150088cb19b9b58fe4061f1cebbe43f9c0db75
Wonko <wonko@hanstool.org>
2024-11-05 20:22:20 +0100

    flake: unify client and ht3

--- flake.nix
+++ flake.nix
@@ -1,54 +1,44 @@
 {
   inputs = {
-        nixpkgs.url = "nixpkgs/nixos-23.11";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-        deps = with pkgs; [
-            python3
-            python3Packages.xlib
-            python3Packages.setuptools
-            python3Packages.tkinter
-            python3Packages.pynvim
-            usbutils
-            neovim-qt
-            git
-        ];
-    in {
-        packages.${system} = rec {
-            ht3-client = pkgs.stdenv.mkDerivation {
-                name = "ht3-client";
-                buildInputs = [ pkgs.python3 ];
-                dontUnpack = true;
-                installPhase = ''
-                    mkdir -p $out/bin
-                    echo "PATH=\"\$PATH:'$PATH'\" PYTHONPATH='$PYTHONPATH:${self}' python -m client \"\$@\"" > $out/bin/ht3-client
-                    chmod +x $out/bin/ht3-client
-                '';
-            };
-            ht3 = pkgs.stdenv.mkDerivation {
-                name = "ht3";
-                buildInputs = deps;
-                dontUnpack = true;
-                installPhase = ''
-                    mkdir -p $out/bin
-                    echo "PATH=\"\$PATH:'$PATH'\" PYTHONPATH='$PYTHONPATH:${self}' python -m ht3 \"\$@\"" > $out/bin/ht3
-                    chmod +x $out/bin/ht3
-                '';
-            };
-            default = ht3;
-        };
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      deps = with pkgs; [
+        python3
+        python3Packages.xlib
+        python3Packages.setuptools
+        python3Packages.tkinter
+        python3Packages.pynvim
+        usbutils
+        neovim-qt
+        git
+      ];
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "hanstool 3";
+        buildInputs = deps;
+        dontUnpack = true;
+        installPhase = ''
+          mkdir -p $out/bin
+          echo "PATH=\"\$PATH:'$PATH'\" PYTHONPATH='$PYTHONPATH:${self}' python -m ht3 \"\$@\"" > $out/bin/ht3
+          chmod +x $out/bin/ht3
+          echo "PATH=\"\$PATH:'$PATH'\" PYTHONPATH='$PYTHONPATH:${self}' python -m client \"\$@\"" > $out/bin/ht3-client
+          chmod +x $out/bin/ht3-client
+        '';
+      };
 
-
-        devShells.${system}.default = pkgs.mkShell {
-            buildInputs = deps;
-            shellHook = ''
-                PYTHONPATH="$PYTHONPATH:${self}"
-                echo 'HT3 Dev Shell ${self} '
-            '';
-        };
-   };
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = deps;
+        shellHook = ''
+          PYTHONPATH="$PYTHONPATH:${self}"
+          echo 'HT3 Dev Shell ${self} '
+        '';
+      };
+    };
 }
